0fa28537515f6340b6b3450676934b4ae7c71037
AlexanderGridinNET <alexander_gridin_dotnet@outlook.com>
2023-05-14 16:16:28 +0300

    copy tab item layout for second task

--- SaprTest/SaprTest/SaprTest.Core/Utils/ViewNames.cs
+++ SaprTest/SaprTest/SaprTest.Core/Utils/ViewNames.cs
@@ -5,4 +5,5 @@ public static class ViewNames
     public const string OutputConsole = "_output_console_";
     public const string ExceptionsConsole = "_exceptions_console_";
     public const string RectanglesCanvas = "_rectangles_canvas_";
+    public const string SecondTaskCanvas = "_second_task_canvas_";
 }
