8248e9e3e5f0f37d199847b10d03cb258b70b5e9
AlexanderGridinNET <alexander_gridin_dotnet@outlook.com>
2023-05-16 12:39:12 +0300

    add some tmp tests

---
+++ SaprTest/SaprTest/Tests/SaprTest.Tests/TmpUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace SaprTest.Tests;
+
+public class TmpUnitTest
+{
+    [Fact]
+    public void Test_MathCos_AlmostZero()
+    {
+        var d = 90d;
+        var r = DegreesToRadians(d); // 1.5707963267948966
+
+        var result = Math.Cos(r); // 6.123233995736766E-17
+
+        Assert.Equal(6.123233995736766E-17, result);
+        Assert.Equal(0.00000_00000_00000_06123_23399_57367_66, result);
+    }
+
+    private static double DegreesToRadians(double d)
+    {
+        return d * Math.PI / 180d;
+    }
+}
